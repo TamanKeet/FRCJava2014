9a496dc0aef13e94c25156246e63ef709a8f6010
Ricardo Delfin <ricardo.delfin.garcia@gmail.com>
2014-02-17 16:26:50 -0600

    Drive and Robot Map repair
    Added the ultrasonic to the DriveSystem and a constant to convert
    Ultrasonic values to inches.

--- src/org/tamankeet3933/java2014/subsystems/DriveSystem.java
+++ src/org/tamankeet3933/java2014/subsystems/DriveSystem.java
@@ -1,5 +1,6 @@
 package org.tamankeet3933.java2014.subsystems;
 
+import edu.wpi.first.wpilibj.AnalogChannel;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -8,13 +9,22 @@ import org.tamankeet3933.java2014.RobotMap;
 /**
  * Subsystem in charge of moving the chassis wheels.
  * @author Ricardo Delfin
- * @version 1.0
+ * @version 1.1
  */
 public class DriveSystem extends Subsystem {
     
-    Jaguar leftWheel = RobotMap.leftWheelMotor;
-    Jaguar rightWheel = RobotMap.rightWheelMotor;
-    RobotDrive drive = new RobotDrive(leftWheel, rightWheel);
+    Jaguar leftWheel;
+    Jaguar rightWheel;
+    RobotDrive drive;
+    AnalogChannel ultrasonic;
+    
+    public DriveSystem()
+    {
+        ultrasonic = RobotMap.ultrasonic;
+        leftWheel = RobotMap.leftWheelMotor;
+        rightWheel = RobotMap.rightWheelMotor;
+        drive = new RobotDrive(leftWheel, rightWheel);
+    }
     
     public void initDefaultCommand() {
         
@@ -23,5 +33,10 @@ public class DriveSystem extends Subsystem {
     public void drive(int moveVal, int rotateVal)
     {
         drive.arcadeDrive(moveVal, rotateVal);
+    }
+    
+    public double getUltrasonic()
+    {
+        return ultrasonic.getAverageVoltage() * RobotMap.ULTRASONIC_TO_INCHES;
     }
 }

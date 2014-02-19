823722e6db9fb754bca2a07482bc001d1fd6d843
analau97gr <analau_97gr@hotmail.com>
2014-02-18 18:46:26 -0600

    Shooter Start and Stop
    +The ShooterStart and ShooterStop command to fire with the shooter
    subsystem

---
+++ src/org/tamankeet3933/java2014/commands/raw/StartShooter.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.tamankeet3933.java2014.commands.raw;
+import org.tamankeet3933.java2014.commands.CommandBase;
+
+/**
+ *
+ * @author analauragarciarivera
+ */
+public class StartShooter extends CommandBase {
+  
+   private double speed;
+   private boolean stopped;
+    
+    public StartShooter(double speed) {
+        requires(shooterSystem);
+        this.speed = speed;
+        this.stopped = false;
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis); 
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+        shooterSystem.start(speed);
+        
+       
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        stopped = true;
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return stopped;
+        
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
---
+++ src/org/tamankeet3933/java2014/commands/raw/StopShooter.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.tamankeet3933.java2014.commands.raw;
+import org.tamankeet3933.java2014.commands.CommandBase;
+
+/**
+ *
+ * @author analauragarciarivera
+ */
+public class StopShooter extends CommandBase {
+   private boolean stopped;
+    
+    public StopShooter(double speed) {
+        requires(shooterSystem);
+        this.stopped= true;
+        
+        
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+        shooterSystem.stop();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        stopped= true;
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false; 
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
